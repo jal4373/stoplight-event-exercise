71a64f8ce536e3c901dfb7e70ffd76683804a36f
Jake Lewis <jakeaustinlewis@gmail.com>
2018-05-21 18:56:20 -0500

    files are up to date

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   // YOUR CODE HERE
@@ -10,49 +10,49 @@
   let goLightOn = false;
 
 
-    //mouse
+  //mouse
 
-  stopButton.addEventListener("mouseenter", function(event) {
+  stopButton.addEventListener("mouseenter", function (event) {
     console.log("Entered", event.target.textContent, "button");
   });
 
-  stopButton.addEventListener("mouseleave", function(event) {
+  stopButton.addEventListener("mouseleave", function (event) {
     console.log("Left", event.target.textContent, "button");
   });
 
-  slowButton.addEventListener("mouseenter", function(event) {
+  slowButton.addEventListener("mouseenter", function (event) {
     console.log("Entered", event.target.textContent, "button");
   });
 
-  slowButton.addEventListener("mouseleave", function(event) {
+  slowButton.addEventListener("mouseleave", function (event) {
     console.log("Left", event.target.textContent, "button");
   });
 
-  goButton.addEventListener("mouseenter", function(event) {
+  goButton.addEventListener("mouseenter", function (event) {
     console.log("Entered", event.target.textContent, "button");
   });
 
-  goButton.addEventListener("mouseleave", function(event) {
+  goButton.addEventListener("mouseleave", function (event) {
     console.log("Left", event.target.textContent, "button");
   });
 
-  document.getElementById('controls').addEventListener('click', function(e) {
+  document.getElementById('controls').addEventListener('click', function (e) {
 
-    if(e.target && e.target.id=='stopButton') {
-      let stopLight = document.getElementById('stopLight'); 
+    if (e.target && e.target.id == 'stopButton') {
+      let stopLight = document.getElementById('stopLight');
       if (stopLightOn) {
         stopLightOn = false;
         console.log(event.target.textContent, "bulb off");
         return stopLight.classList.remove("stop");
-  
+
       } else {
         stopLightOn = true;
         console.log(event.target.textContent, "bulb on");
         return stopLight.classList.add("stop");
-        }
-    
-    } else if(e.target && e.target.id=='slowButton') {
-      let goLight = document.getElementById('goLight'); 
+      }
+
+    } else if (e.target && e.target.id == 'slowButton') {
+      let goLight = document.getElementById('goLight');
       if (goLightOn) {
         goLightOn = false;
         console.log(event.target.textContent, "bulb off");
@@ -64,11 +64,9 @@
         return goLight.classList.add("go");
       }
 
-    } else if(e.target && e.target.id=='goButton') {
+    } else if (e.target && e.target.id == 'goButton') {
 
-    
-
-      let goLight = document.getElementById('goLight'); 
+      let goLight = document.getElementById('goLight');
       if (goLightOn) {
         goLightOn = false;
         console.log(event.target.textContent, "bulb off");
